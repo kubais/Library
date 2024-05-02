35f047a0e7a2f0ea7ef3f24a6f9afbc3215ee16e
kubais <30256909+kubais@users.noreply.github.com>
2024-05-02 09:18:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book() {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.state = state;
+}
+
+function addBookToLibrary() {
+    
+}
